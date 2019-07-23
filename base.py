71d4a1f12f2c265aa05bf9e25f46612bdf8098a3
Richardson <6871347@basco.com>
2019-07-23 15:00:14 -0500

    added file name creation and creation of DocPrimary (Primary HTML doc)

--- base.py
+++ base.py
@@ -1,14 +1,21 @@
     #import dependent modules
-import os,time,random,fileinput
-    #intialize variable set 0
+import os, time, random, fileinput
+    #intialize variable set 0.\
 global ImageNum
-    #image number ex:117339, 117339_1
+    #image number ex:117339, 117339_1.\
 global DocPrimary, DocSecondary, DocTertiary
-    #the 3 different documents generated and used. DocPrimary is everything inserted, DocSecondary is base html.
-    # DocTertiary is html inserted and modifed by python into DocSecondary.
+    #the 3 different documents generated and used. DocPrimary is everything inserted, DocSecondary is base html.\
+    # DocTertiary is html inserted and modifed by python into DocSecondary.\
 global DocTimeStamp
     #timestamp taken at time of document generation in miliseconds
-global NamePrompt, ImageDir, OutputDir
+global DocName, ImageDir, OutputDir, DocNameFinal
     #NamePrompt (prompt for custom document name on output)\
     #ImageDir (prompt for directory to all the images to be used)\
     #OutputDir (Directory that the final document will be output to)\
+DocName=input("Document name")
+print (DocName)
+
+DocNameFinal =DocName + "_" + str(time.time())+".html"
+print(DocNameFinal)
+
+open(DocNameFinal,"w+")
